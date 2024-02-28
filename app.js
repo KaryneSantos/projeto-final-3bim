1c12994342c4317e49abc9fa54b6eb6386100257
Karyne Santos <karynesantos0901@gmail.com>
2024-02-28 20:48:38 -0300

    adicionando arquivos do projeto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const port = 3000;
+
+// Pagina da Listagem de usuários
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+app.listen(port);
+console.log('iniciou...');
